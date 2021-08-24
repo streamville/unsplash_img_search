1ff342e02dda11679cb7544ea322d3ac1dbb0eb0
Dennie Nguyen <streamvillecanada@gmail.com>
2021-08-23 22:39:15 -0400

    render images from Unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import unsplash from '../api/unsplash';
+import ImageList from './ImageList';
 
 class App extends React.Component {
   state = { images: [] };
@@ -18,10 +19,17 @@ class App extends React.Component {
 
   render(){
     return(
-      <div class="ui container" style={{ marginTop: '10px'}}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images.
+
+      <React.Fragment>
+        <div class="ui container">
+          <h1>Unsplash Image Search</h1>
+        </div>
+        <div class="ui container" style={{ marginTop: '10px'}}>
+          <SearchBar onSubmit={this.onSearchSubmit} />
+          Found: {this.state.images.length} images.
+          <ImageList images={this.state.images} />
       </div>
+      </React.Fragment>
     )
   }
 }
